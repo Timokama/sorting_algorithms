5978403babc3e8a8ae3e551b33b0f1e2e5a244ad
Timothy Kamau <kamautimothy635@gmail.com>
2022-11-09 05:49:58 +0000

    select sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,7 +15,7 @@ void selection_sort(int *array, size_t size)
 
 	if (array == NULL || size < 1 )
 		return;
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		minIndex = i;
 		for (j = i + 1; j < size; j++)
